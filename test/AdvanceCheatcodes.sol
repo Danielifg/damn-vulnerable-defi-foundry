8113250499e8ce6cda266ae4fdc32408578166ac
Daniel G <danielifguerrero@gmail.com>
2023-04-06 15:18:30 -0400

    init puppet v3

--- test/AdvanceCheatcodes.sol
+++ test/AdvanceCheatcodes.sol
@@ -4,7 +4,6 @@ pragma solidity >=0.8.0;
 import "forge-std/Test.sol";
 import {DamnValuableToken} from "../src/Contracts/DamnValuableToken.sol";
 
-
 contract Vault {
     bool public s_locked;
     bytes32 private s_password;
@@ -27,26 +26,23 @@ contract AdvanceCheatcodes is Test {
 
     DamnValuableToken internal erc20;
     Vault public vault;
-    
-    function setUp() public{
+
+    function setUp() public {
         erc20 = new DamnValuableToken();
         vault = new Vault("0x124__400x0");
     }
 
     function test_writeBalance() public {
         // set token balance to 1e18 and update totalSupply()
-        deal(address(erc20),address(this),10e18,true);
+        deal(address(erc20), address(this), 10e18, true);
     }
-    function test_writeArbitrary() public{f
-        stdstore
-            .target(address(vault))
-            .sig(vault.a.selector)
-            .checked_write(100);
-        assertEq(vault.a(),100);
+
+    function test_writeArbitrary() public {
+        stdstore.target(address(vault)).sig(vault.a.selector).checked_write(100);
+        assertEq(vault.a(), 100);
     }
-    
+
     function invariant_CannotUnlockVault() public view {
         assert(vault.s_locked());
     }
-
 }
